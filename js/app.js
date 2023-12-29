d9877375a43bfba60466ee1762ac377e65980d4c
Samir Ramos <dev.samir.ramos@gmail.com>
2023-12-29 18:46:52 -0300

    Praticamente tudo alterado

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
+const btnEnter = document.querySelector("#enter__game");
+const gameHistory = document.querySelector("#game__history");
+
 const acceptName = document.querySelector("#enter__name");
 const playerName = document.querySelector("#player__name");
 const userName = document.querySelector("#username");
 const playerEntry = document.querySelector("#player__entry");
 const gameBoard = document.querySelector("#game__board");
+
+btnEnter.onclick = function () {
+  gameHistory.style.display = "none";
+  playerEntry.style.display = "flex";
+};
 
 acceptName.onclick = function () {
   playerEntry.style.display = "none";
