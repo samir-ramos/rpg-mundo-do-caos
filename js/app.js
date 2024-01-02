c376d415a52b6a59bbe63b2bd33ed1aeba840de8
Samir Ramos <dev.samir.ramos@gmail.com>
2024-01-01 22:25:43 -0300

    Some JS

--- js/app.js
+++ js/app.js
@@ -1,30 +1,53 @@
-const gameHistory = document.getElementById("game__history");
-const playerEntry = document.getElementById("player__entry");
-const gameActions = document.getElementById("game__actions");
-const namePicked = document.getElementById("username");
-const playerName = document.getElementById("player__name");
 const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 const button3 = document.getElementById("button3");
+const raceButtons = document.querySelectorAll("#race__selection .btn__race");
+let race = null;
+
+const gameHistory = document.getElementById("game__history");
+const playerEntry = document.getElementById("player__entry");
+const gameActions = document.getElementById("game__actions");
+const userName = document.getElementById("username");
+const invalidEntry = document.getElementById("invalid__entry");
+
+function starGame() {
+  gameHistory.style.display = "grid";
+  playerEntry.style.display = "none";
+  gameActions.style.display = "none";
+  button1.innerText = "ENTRAR";
+  button2.innerText = "INFORMAÇÕES";
+  button3.innerText = "";
+}
+
+starGame();
 
 button1.addEventListener("click", function () {
-  if (gameHistory.style.display !== "none") {
+  if (gameHistory.style.display === "grid") {
     gameHistory.style.display = "none";
     playerEntry.style.display = "grid";
     gameActions.style.display = "none";
     button1.innerText = "ACEITAR";
-    button2.innerText = "APAGAR";
+    button2.innerText = "LIMPAR";
     button3.innerText = "VOLTAR";
-  } else if (playerEntry.style.display !== "none") {
-    gameHistory.style.display = "none";
-    playerEntry.style.display = "none";
-    gameActions.style.display = "grid";
-    playerName.innerText = namePicked.value;
-    button1.innerText = "LOJA";
-    button1.style.backgroundColor = "salmon";
-    button2.innerText = "FLORESTA";
-    button2.style.backgroundColor = "darkgreen";
-    button3.innerText = "MISSÃO";
-    button3.style.backgroundColor = "orange";
+  } else if (playerEntry.style.display === "grid") {
+    if (
+      userName.value == null ||
+      userName.value == "" ||
+      userName.value == undefined
+    ) {
+      gameHistory.style.display = "none";
+      playerEntry.style.display = "grid";
+      gameActions.style.display = "none";
+      invalidEntry.style.display = "block";
+    } else {
+      gameHistory.style.display = "none";
+      playerEntry.style.display = "grid";
+      gameActions.style.display = "none";
+      button1.innerText = "ACEITAR";
+      button2.innerText = "LIMPAR";
+      button3.innerText = "VOLTAR";
+    }
   }
 });
+
+function choseRace(button) {}
